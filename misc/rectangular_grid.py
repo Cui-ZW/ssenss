909c2d822ea2eefb80419b270342fd2c9bc007b2
Christopher Nilsen <research@christophernilsen.no>
2015-01-09 16:25:13 +0100

    Chebyshev grid stretching added to grid script
    The script rectangular_grid.py now includes both equidistant and Chebyshev
    element grids.

--- misc/rectangular_grid.py
+++ misc/rectangular_grid.py
@@ -26,19 +26,20 @@ def write_parameters(fp, n, nel):
 	dat[0]["nel"] = nel
 	write_dataset(fp, dat, "Parameters")
 
-def write_elements(fp, n, nel, ind_order, xmin, xmax, bc):
+def write_elements(fp, n, nel, grid_func, ind_order, xmin, xmax, bc):
 	dat = new_elements(n, nel[0] * nel[1] * nel[2])
-	set_element_values(dat, nel, ind_order, xmin, xmax)
+	set_element_values(dat, nel, grid_func, ind_order, xmin, xmax)
 	set_boundary_conditions(dat, nel, ind_order, bc)
 	write_dataset(fp, dat, "Element array")
 
-def set_element_values(dat, nel, ind_order, xmin, xmax):
+def set_element_values(dat, nel, grid_func, ind_order, xmin, xmax):
 	for k in range(nel[2]):
 		for j in range(nel[1]):
 			for i in range(nel[0]):
 				ind = np.array([i, j, k])
 				el = elnum(ind[ind_order], nel[ind_order])
-				(x, y, z) = cartesian_grid(ind, nel, xmin, xmax)
+				(x, y, z) = gridpoints(ind, nel, xmin, xmax,
+						grid_func)
 				dat[el]["X"][:] = x
 				dat[el]["Y"][:] = y
 				dat[el]["Z"][:] = z
@@ -57,8 +58,8 @@ def set_boundary_conditions(dat, nel, ind_order, bc):
 		if bc[s] < 0:
 			k = s // 2
 			l = s % 2
-			for j in range(np.roll(nel, -k)[1]):
-				for i in range(np.roll(nel, -k)[2]):
+			for j in range(np.roll(nel, -k)[2]):
+				for i in range(np.roll(nel, -k)[1]):
 					ind = np.roll(np.array([l *
 							(nel[k] - 1), i, j]), k)
 					el = elnum(ind[ind_order],
@@ -82,13 +83,32 @@ def elnum(i, n):
 def perind(i, n):
 	return i - (i // n) * n
 
-def cartesian_grid(i, n, xmin, xmax):
-	xa = xmin + i * (xmax - xmin) / n
-	xb = xmin + (i + 1) * (xmax - xmin) / n
-	xgrid = xa[0] + (xb[0] - xa[0]) * np.array([0, 1, 0, 1, 0, 1, 0, 1])
-	ygrid = xa[1] + (xb[1] - xa[1]) * np.array([0, 0, 1, 1, 0, 0, 1, 1])
-	zgrid = xa[2] + (xb[2] - xa[2]) * np.array([0, 0, 0, 0, 1, 1, 1, 1])
+def gridpoints(i, n, xmin, xmax, grid_func):
+	(xa, xb) = grid_func[0](i[0], n[0], xmin[0], xmax[0])
+	(ya, yb) = grid_func[1](i[1], n[1], xmin[1], xmax[1])
+	(za, zb) = grid_func[2](i[2], n[2], xmin[2], xmax[2])
+	xgrid = xa + (xb - xa) * np.array([0, 1, 0, 1, 0, 1, 0, 1])
+	ygrid = ya + (yb - ya) * np.array([0, 0, 1, 1, 0, 0, 1, 1])
+	zgrid = za + (zb - za) * np.array([0, 0, 0, 0, 1, 1, 1, 1])
 	return (xgrid, ygrid, zgrid)
+
+def equidistant_grid(i, n, xmin, xmax):
+	return (xmin + i * (xmax - xmin) / n,
+			xmin + (i + 1) * (xmax - xmin) / n)
+
+def chebyshev_node(i, n, xmin, xmax):
+	tmp = np.cos((2.0 * (i + 1) - 1.0) * np.pi / (2.0 * n))
+	return 0.5 * (xmin + xmax) - 0.5 * (xmax - xmin) * tmp
+
+def chebyshev_grid(i, n, xmin, xmax):
+	cn = [chebyshev_node(j, n, xmin, xmax) for j in [i - 1, i, i + 1]]
+	a = 0.5 * (cn[0] + cn[1])
+	b = 0.5 * (cn[1] + cn[2])
+	if i == 0:
+		a = xmin
+	elif i == (n - 1):
+		b = xmax
+	return (a, b)
 
 def neighbours(i, n, ind_order):
 	neigh = np.zeros(6)
@@ -128,12 +148,16 @@ def read_user_input():
 	nelx = int(input("Number of elements in x direction, nelx = "))
 	nely = int(input("Number of elements in y direction, nely = "))
 	nelz = int(input("Number of elements in z direction, nelz = "))
+	grid_type = input("XYZ grid spacing ('e' for equdistant or 'c' for "
+			"Chebyshev, e.g. 'eec'): ")
+	grid_function = [{'e': equidistant_grid, 'c': chebyshev_grid}[i] for i
+			in grid_type]
 	ind_order = input("Element index order ('xyz', 'xzy', 'yxz', 'yzx',"
 			" 'zxy' or 'zyx): ")
+	num_ind_order = [{'x': 0, 'y': 1, 'z': 2}[i] for i in ind_order]
 
 	print("\nPlease specify boundary conditions ('n' for no-slip,"
 			" 'f' for free-slip and 'p' for periodic)")
-	num_ind_order = [{'x': 0, 'y': 1, 'z': 2}[i] for i in ind_order]
 	bc_text = ("{0} ({1}-direction) boundary condition: ")
 	bc = [input(bc_text.format("West", "x")), input(bc_text.format("East",
 			"x")), input(bc_text.format("South", "y")),
@@ -144,15 +168,15 @@ def read_user_input():
 	filename = input("\nFilename: ")
 
 	return (np.array([xmin, ymin, zmin]), np.array([xmax, ymax, zmax]),
-			np.array([nelx, nely, nelz]), num_ind_order, bc_num,
-			filename)
+			np.array([nelx, nely, nelz]), grid_function,
+			num_ind_order, bc_num, filename)
 
 def main():
 	n = 2
-	xmin, xmax, nel, ind_order, bc, filename = read_user_input()
+	xmin, xmax, nel, grid_func, ind_order, bc, filename = read_user_input()
 	f = h5py.File(filename, "w")
 	write_parameters(f, n, nel[0] * nel[1]* nel[2])
-	write_elements(f, n, nel, ind_order, xmin, xmax, bc)
+	write_elements(f, n, nel, grid_func, ind_order, xmin, xmax, bc)
 	f.close()
 
 if __name__ == "__main__":
